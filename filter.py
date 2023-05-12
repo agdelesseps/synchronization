57d351448edd90c5a6e3514b59983a5da0123c78
agdelesseps <agdelesseps@gmail.com>
2023-05-12 10:00:54 -0400

    Fix inputs bug

--- filter.py
+++ filter.py
@@ -43,8 +43,8 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         main(sys.argv[1])
     elif len(sys.argv) == 3:
-        main(sys.argv[1], int(sys.argv[2]))
+        main(sys.argv[1], (int(sys.argv[2]),))
     elif len(sys.argv) == 4:
-        main(sys.argv[1], int(sys.argv[2]), (int(sys.argv[3]),))
+        main(sys.argv[1], (int(sys.argv[2]), int(sys.argv[3])))
     else:
         print("ERROR: wrong # arguments")
